8953cc99718d49769162de8fe48d23901fb4a066
nodeJayS <shinjayw@gmail.com>
2020-12-04 23:43:14 -0500

    Location controller unit tests

--- Aqua.UnitTests/LocationTests.cs
+++ Aqua.UnitTests/LocationTests.cs
@@ -21,8 +21,8 @@ namespace Aqua.UnitTests
             var mockRepo = new Mock<ILocationRepo>();
             var locationList = new List<Location>();
             var cali = new Location();
-            cali.Id = 1;
-            cali.City = "cali";
+                cali.Id = 1;
+                cali.City = "cali";
             locationList.Add(cali);
             mockRepo.Setup(r => r.GetAllLocations())
                 .Returns
@@ -34,6 +34,65 @@ namespace Aqua.UnitTests
             var viewResult = Assert.IsAssignableFrom<ViewResult>(actionResult);
             var locations = Assert.IsAssignableFrom<IEnumerable<LocationViewModel>>(viewResult.Model).ToList();
             Assert.Equal("cali", locations[0].City);
+            Assert.Equal(1, locations[0].Id);
+        }
+        [Fact]
+        public void Index_Get_Multiple_Locations()
+        {
+            // Arrange
+            var mockRepo = new Mock<ILocationRepo>();
+            var locationList = new List<Location>();
+            var cali = new Location();
+                cali.Id = 1;
+                cali.City = "cali";
+            var hk = new Location();
+                hk.Id = 2;
+                hk.City = "hongkong";
+            locationList.Add(cali);
+            locationList.Add(hk);
+            mockRepo.Setup(r => r.GetAllLocations())
+                .Returns
+                    (locationList);
+            var controller = new LocationController(new NullLogger<LocationController>(), mockRepo.Object, null, null);
+            // Act
+            IActionResult actionResult = controller.Index();
+            // Assert
+            var viewResult = Assert.IsAssignableFrom<ViewResult>(actionResult);
+            var locations = Assert.IsAssignableFrom<IEnumerable<LocationViewModel>>(viewResult.Model).ToList();
+            Assert.Equal(2, locations.Count()); // Number of objects in location list
+            Assert.Equal(2, locations[1].Id); // Return Id of the second location (hk)
+        }
+        [Fact]
+        public void Index_Get_Valid_Location()
+        {
+            // Arrange
+            var mockRepo = new Mock<ILocationRepo>();
+            var locationList = new List<Location>();
+            var cali = new Location();
+            cali.Id = 1;
+            locationList.Add(cali);
+            mockRepo.Setup(r => r.GetAllLocations())
+                .Returns
+                    (locationList);
+            var controller = new LocationController(new NullLogger<LocationController>(), mockRepo.Object, null, null);
+            // Act
+            IActionResult actionResult = controller.Index();
+            // Assert
+            Assert.True(controller.ModelState.IsValid);
+        }
+        [Fact]
+        public void Index_Create_Location()
+        {
+            // Arrange
+            var mockRepo = new Mock<ILocationRepo>();
+            var hk = new Location();
+                hk.Id = 1;
+                hk.City = "hongkong";
+            var controller = new LocationController(new NullLogger<LocationController>(), mockRepo.Object, null, null);
+            // Act
+            controller.Create(hk);
+            // Assert
+            Assert.True(controller.ModelState.IsValid);
         }
     }
 }
